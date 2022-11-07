48f8d62ed2b6273c3493a68ee98a01c130e67dc2
antoniapisano <antoniapisano@campus.masterschool.com>
2022-11-07 22:46:15 +0000

    first udacity submission confirmed rubric

---
+++ README.md
@@ -0,0 +1,3 @@
+The purpose of this Udacity project is to become familiar with React and understand its functionality, components, props and states. It also introduces us to APIs through the provided BooksAPI which teaches how to fetch specific data to use dynamically.
+
+In order to install this react App you simply need to clone the repository from github. Once you have done this open the folder in VSCode (or whichever similar application you use) and run "npm install" on the terminal. Once you have done this run "npm start" and this will launch the app.
---
+++ src/App.js
@@ -0,0 +1,131 @@
+import "./App.css";
+import Shelves from "./Shelves";
+import { useState, useEffect } from "react";
+import * as BooksAPI from "./BooksAPI";
+import Book from "./Book";
+
+// TODO: define the App function which is the root of the whole app and create the required props and states.
+
+function App() {
+  const [showSearchPage, setShowSearchpage] = useState(false);
+  const [books, setBooks] = useState([]);
+  const [query, setQuery] = useState("");
+  const [searchBooks, setSearchBooks] = useState([]);
+  const [joinBooks, setJoinBooks] = useState([]);
+  const [mapBooks, setMapBooks] = useState(new Map());
+
+// TODO: access data from the BooksAPI that Udacity provided. This is necessary for the book searches.
+
+  useEffect(() => {
+    BooksAPI.getAll().then((data) => {
+      setBooks(data);
+      setMapBooks(createMapBooks(data));
+    });
+  }, []);
+
+// TODO: Map over all the books provided in the API to select the correct ones according to the search
+
+  useEffect(() => {
+    const combined = searchBooks.map((book) => {
+      if (mapBooks.has(book.id)) {
+        return mapBooks.get(book.id);
+      } else {
+        return book;
+      }
+    });
+    setJoinBooks(combined);
+  }, [searchBooks]);
+
+// TODO: Makes the search page blank if there are no successful search matches and restores it 
+// to blank when the search is complete.
+
+  useEffect(() => {
+    let isActive = true;
+    if (query) {
+      BooksAPI.search(query).then((data) => {
+        if (data.error) {
+          setSearchBooks([]);
+        } else {
+          if (isActive) {
+            setSearchBooks(data);
+          }
+        }
+      });
+    }
+
+    return () => {
+      isActive = false;
+      setSearchBooks([]);
+    };
+  }, [query]);
+
+  const createMapBooks = (books) => {
+    const map = new Map();
+    books.map((book) => map.set(book.id, book));
+    return map;
+  };
+
+// TODO: Updates which shelf the book is on and keeps the state when browser is refreshed.
+
+  const updateBookShelf = (book, whereTo) => {
+    const updatedBooks = books.map((b) => {
+      if (b.id === book.id) {
+        book.shelf = whereTo;
+        return book;
+      }
+      return b;
+    });
+    setBooks(updatedBooks);
+    BooksAPI.update(book, whereTo);
+  };
+
+  return (
+    <div className="app">
+      {showSearchPage ? (
+        <div className="search-books">
+          <div className="search-books-bar">
+            <a
+              className="close-search"
+              onClick={() => setShowSearchpage(!showSearchPage)}
+            >
+              Close
+            </a>
+            <div className="search-books-input-wrapper">
+              <input
+                type="text"
+                placeholder="Search by title or author"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+              />
+            </div>
+          </div>
+          <div className="search-books-results">
+            <ol className="books-grid">
+              {joinBooks.map((b) => (
+                <li key={b.id}>
+                  <Book book={b} changeBookShelf={updateBookShelf} />
+                </li>
+              ))}
+            </ol>
+          </div>
+        </div>
+      ) : (
+        <div className="list-books">
+          <div className="list-books-title">
+            <h1>MyReads</h1>
+          </div>
+          <div className="list-books-content">
+            <div>
+              <Shelves books={books} updateBookShelf={updateBookShelf} />
+            </div>
+          </div>
+          <div className="open-search">
+            <a onClick={() => setShowSearchpage(!showSearchPage)}>Add a book</a>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
